b739f2d3c8e27a58656f1f3931e559781691b8a9
Izzu Zantya Fawwas <49353967+izzuzantyaf@users.noreply.github.com>
2020-04-20 16:49:37 +0700

    insert writer's identity and social media

---
+++ 00 - Template/Main.py
@@ -0,0 +1,8 @@
+"""
+Written by Izzu Zantya Fawwas
+Github : https://github.com/izzuzantyaf
+Instagram : https://instagram.com/izzuzantyaf
+Twitter : https://twitter.com/izzuzantyaf
+Facebook : https://facebook.com/izzuzantyaf
+Tiktok : @izzuzantyaf
+"""
