88ff59e2c63f2a591e017f360108824a4440977d
Izzu Zantya Fawwas <49353967+izzuzantyaf@users.noreply.github.com>
2020-04-20 16:51:01 +0700

    insert email

--- 00 - Template/Main.py
+++ 00 - Template/Main.py
@@ -1,5 +1,6 @@
 """
 Written by Izzu Zantya Fawwas
+Email : izzuzantyaf@gmail.com
 Github : https://github.com/izzuzantyaf
 Instagram : https://instagram.com/izzuzantyaf
 Twitter : https://twitter.com/izzuzantyaf
